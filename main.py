9c3b66e949d5dab61dcd8dfb5d8ee43f6398f252
SomTron <sommeregger@gmail.com>
2025-11-19 15:27:07 +0100

    Laufzeit repariert, Übergangszeit am Abend. Kein Healthcheck rebase2

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ from dateutil.relativedelta import relativedelta
 from telegram_handler import (send_telegram_message, send_welcome_message, telegram_task, get_runtime_bar_chart,
                               get_boiler_temperature_history, deaktivere_urlaubsmodus, is_solar_window)
 
-#rebase
+#rebase2
 
 # Basisverzeichnis für Temperatursensoren und Sensor-IDs
 BASE_DIR = "/sys/bus/w1/devices/"
